f300659317dea6fe5f763bd085132e31d0217d96
OscarVsp <boscar.vs98@gmail.com>
2025-06-19 18:38:59 +0200

    add in-encalve time reading

--- examples/enclave-latency/eapp/enclave_latency.c
+++ examples/enclave-latency/eapp/enclave_latency.c
@@ -1,6 +1,17 @@
+#include <time.h>
+#include <stdio.h>
+#include <errno.h>
 
 int main()
 {
-  asm("nop");
+
+  struct timespec now;
+  int ret;
+  ret = clock_gettime(CLOCK_MONOTONIC, &now);
+  if (ret != 0) {
+    printf("Error getting time. error code: %d\n", errno);
+    return -1;
+  }
+  printf("%ld,%ld\n", now.tv_sec, now.tv_nsec);
   return 0;
 }
