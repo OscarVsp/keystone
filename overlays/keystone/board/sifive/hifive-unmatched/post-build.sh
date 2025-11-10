5fda1a8ab4243b45319e7cf7f4a7a40abe616f4e
OscarVsp <boscar.vs98@gmail.com>
2025-11-10 10:26:50 +0100

    fix slow build by caching udev rules and systemd units

--- overlays/keystone/board/sifive/hifive-unmatched/post-build.sh
+++ overlays/keystone/board/sifive/hifive-unmatched/post-build.sh
@@ -9,6 +9,24 @@ cp $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/extlinux.conf $TARG
 # Install udev rules & systemd units
 mkdir -p $TARGET_DIR/usr/lib/systemd/system
 mkdir -p $TARGET_DIR/etc/udev/rules.d
-wget -P "${TARGET_DIR}/etc/udev/rules.d" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-udev-rules/files/unmatched/99-pwm-leds.rules"
-wget -P "${TARGET_DIR}/usr/lib/systemd/system" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-systemd-units/files/led-bootstate-green.service"
-wget -P "${TARGET_DIR}/usr/lib/systemd/system" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-systemd-units/files/led-bootstate-green.timer"
+# Install udev rules & systemd units
+mkdir -p $TARGET_DIR/usr/lib/systemd/system
+mkdir -p $TARGET_DIR/etc/udev/rules.d
+
+if [ ! -d $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/sdk/rules.d ]; then
+    wget -P "${BR2_EXTERNAL_KEYSTONE_PATH}/board/sifive/hifive-unmatched/sdk/rules.d" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-udev-rules/files/unmatched/99-pwm-leds.rules"
+fi
+
+cp -r $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/sdk/rules.d $TARGET_DIR/etc/udev/rules.d
+
+if [ ! -f $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/sdk/led-bootstate-green.service ]; then
+    wget -P "${BR2_EXTERNAL_KEYSTONE_PATH}/board/sifive/hifive-unmatched/sdk/led-bootstate-green.service" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-systemd-units/files/led-bootstate-green.service"
+fi
+
+cp $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/sdk/led-bootstate-green.service $TARGET_DIR/usr/lib/systemd/system
+
+if [ ! -f $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/sdk/led-bootstate-green.timer ]; then
+    wget -P "${BR2_EXTERNAL_KEYSTONE_PATH}/board/sifive/hifive-unmatched/sdk/led-bootstate-green.timer" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-systemd-units/files/led-bootstate-green.timer"
+fi
+
+cp $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/sdk/led-bootstate-green.timer $TARGET_DIR/usr/lib/systemd/system
